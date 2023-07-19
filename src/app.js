79c6854fe0145bc3c50999d2da73f9c57f7c9b57
ouhab <mohamed-moh98@live.com>
2023-07-19 11:07:10 +0100

    update repo to host on vercel

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-import serverless from "serverless-http";
+
 import userRouter from "./routes/user-routes.js";
 import episodeRouter from "./routes/episode-routes.js";
 const app = express();
@@ -14,18 +14,17 @@ mongoose.connect(process.env.MONGO_URL)
         .catch((e) => console.log(e));
 
 
-        app.get("/api/test", () => {
+        app.get("/test", () => {
             console.log("test is succ")
         })
 
 
 app.use(express.json())
-app.use("/api/user",userRouter);
-app.use("/api/episode", episodeRouter);
+app.use("user",userRouter);
+app.use("episode", episodeRouter);
 app.listen(process.env.PORT || 5000, () =>{
     console.log("backend is running");
 });
 
 // Export the app and the serverless function
-// export default app;
-// exports.handler = serverless(app);
+export default app;
