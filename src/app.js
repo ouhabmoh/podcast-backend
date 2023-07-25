d9146d0bf9cde2d9a19bec9e410fd5f70c31a5e8
ouhab <mohamed-moh98@live.com>
2023-07-25 22:54:45 +0100

    add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 
 import userRouter from "./routes/user-routes.js";
 import episodeRouter from "./routes/episode-routes.js";
@@ -21,7 +21,8 @@ mongoose.connect("mongodb://ismmoh:wPLMCu9SObXxfK8a@ac-bp6oqvh-shard-00-00.3x6ga
             console.log("test is succ")
         })
 
-
+// enabling CORS for any unknown origin(https://xyz.example.com)
+app.use(cors());
 app.use(express.json({ limit: '500mb' }))
 app.use('/resources',express.static('resources'))
 app.use("/users",userRouter);
