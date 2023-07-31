83042690c97fc55837b69d4ee64c299e26f00c53
ouhab <mohamed-moh98@live.com>
2023-07-31 20:47:22 +0100

    fix deploy

--- src/auth.js
+++ src/auth.js
@@ -1,33 +1,35 @@
 import passport from "./passportConfig.js";
 export const isAdmin = (req, res, next) => {
     // passport.authenticate('jwt', { session: false }, (err, user) => {
-        console.log(user);
-        if (err) {
-            console.log(err);
-            return res.status(401).json({ message: 'Unauthorized' });
-          }
-          if(!user) {
-            return res.status(401).json({ message: 'Unauthorized' });
-          }
+    //     console.log(user);
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(401).json({ message: 'Unauthorized' });
+    //       }
+    //       if(!user) {
+    //         return res.status(401).json({ message: 'Unauthorized' });
+    //       }
           
-          if (user.role !== 'admin') {
-            return res.status(403).json({ message: 'Forbidden - Admin access required' });
-          }
+    //       if (user.role !== 'admin') {
+    //         return res.status(403).json({ message: 'Forbidden - Admin access required' });
+    //       }
   
-      req.user = user;
-    //   next();
-    // })(req, res, next);
+    //   req.user = user;
+    // //   next();
+    // })
+    (req, res, next);
   };
 
   export const notAdmin = (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, (err, user) => {
+    // passport.authenticate('jwt', { session: false }, (err, user) => {
        
-      //     if (!user || user.role !== 'admin') {
-      //       req.query.isPublished = '1';
-      //     }
+    //   //     if (!user || user.role !== 'admin') {
+    //   //       req.query.isPublished = '1';
+    //   //     }
   
-      // req.user = user;
-      next();
-    })(req, res, next);
+    //   // req.user = user;
+    //   next();
+    // })
+    (req, res, next);
   };
   
