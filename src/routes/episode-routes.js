6241995b34e690c4191102888d0b8162b456978a
ouhab <mohamed-moh98@live.com>
2023-07-31 20:50:21 +0100

    fix deploy

--- src/routes/episode-routes.js
+++ src/routes/episode-routes.js
@@ -50,20 +50,36 @@ import {isAdmin, notAdmin} from "../auth.js";
 import { getAllEpisodes, addEpisode, getLastEpisodeNumber, toggleIsPublished, getEpisodeNeighbours, updateEpisode, getById, deleteEpisodeById, getAudioById, addNote, deleteNote } from "../controllers/episode-controller.js";
 const episodeRouter = express.Router();
 
-episodeRouter.get("/", notAdmin, getAllEpisodes);
+// episodeRouter.get("/", notAdmin, getAllEpisodes);
+// episodeRouter.get("/last", getLastEpisodeNumber);
+// episodeRouter.get("/neighbors/:episodeNumber", getEpisodeNeighbours);
+// // episodeRouter.post("/", upload.fields([{ name: 'audio' }, { name: 'image' }]), addEpisode);
+// // episodeRouter.patch("/:id",  upload.fields([{ name: 'audio' }, { name: 'image' }]),  updateEpisode);
+// episodeRouter.post("/", isAdmin, episodeValidationRules(), validate, addEpisode);
+// episodeRouter.put("/isPublished/:id", isAdmin, toggleIsPublished);
+// episodeRouter.patch("/:id", isAdmin, updateEpisode);
+// episodeRouter.get("/:id", getById);
+// episodeRouter.get("/:id/audio", getAudioById);
+// episodeRouter.delete("/:id", isAdmin, deleteEpisodeById);
+// episodeRouter.post("/:id/note", isAdmin, addNote);
+
+// episodeRouter.delete("/:id/note/:noteId", isAdmin, deleteNote);
+
+
+episodeRouter.get("/", getAllEpisodes);
 episodeRouter.get("/last", getLastEpisodeNumber);
 episodeRouter.get("/neighbors/:episodeNumber", getEpisodeNeighbours);
 // episodeRouter.post("/", upload.fields([{ name: 'audio' }, { name: 'image' }]), addEpisode);
 // episodeRouter.patch("/:id",  upload.fields([{ name: 'audio' }, { name: 'image' }]),  updateEpisode);
-episodeRouter.post("/", isAdmin, episodeValidationRules(), validate, addEpisode);
-episodeRouter.put("/isPublished/:id", isAdmin, toggleIsPublished);
-episodeRouter.patch("/:id", isAdmin, updateEpisode);
+episodeRouter.post("/",  episodeValidationRules(), validate, addEpisode);
+episodeRouter.put("/isPublished/:id", toggleIsPublished);
+episodeRouter.patch("/:id", updateEpisode);
 episodeRouter.get("/:id", getById);
 episodeRouter.get("/:id/audio", getAudioById);
-episodeRouter.delete("/:id", isAdmin, deleteEpisodeById);
-episodeRouter.post("/:id/note", isAdmin, addNote);
+episodeRouter.delete("/:id",  deleteEpisodeById);
+episodeRouter.post("/:id/note", addNote);
 
-episodeRouter.delete("/:id/note/:noteId", isAdmin, deleteNote);
+episodeRouter.delete("/:id/note/:noteId", deleteNote);
 export default episodeRouter;
 
 
