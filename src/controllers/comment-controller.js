c320c6306961385ec006d13708566e7d8ec0345e
ouhab <mohamed-moh98@live.com>
2023-08-05 20:04:02 +0100

    add getAllComments

--- src/controllers/comment-controller.js
+++ src/controllers/comment-controller.js
@@ -1,29 +1,57 @@
-
 import Comment from "../model/Comment.js";
+import Article from "../model/Article.js";
+import Episode from "../model/Episode.js";
 
-export const updateComment =  async (req, res) => {
-    const commentId = req.params.id;
-    const { content } = req.body;
-    try {
-    // Check if the comment exists
-    const comment = await Comment.findById(commentId);
-    if (!comment) {
-      return res.status(404).json({ message: "Comment not found" });
-    }
+export const getAllComments = async (req, res) => {
+	try {
+		// Get all articles with their comments
+		const articles = await Article.find({
+			comments: { $exists: true, $not: { $size: 0 } },
+		})
+			.select("id articleNumber title comments")
+			.populate("comments", "id content");
+		// Get all episodes with their comments
+		const episodes = await Episode.find({
+			comments: { $exists: true, $not: { $size: 0 } },
+		})
+			.select("id episodeNumber title comments")
+			.populate("comments", "id content");
 
-    // Check if the user is the owner of the comment
-    if (comment.user.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ message: "You are not authorized to update this comment" });
-    }
+		res.status(200).json({
+			articlesComments: articles,
+			episodesComments: episodes,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
 
-    // Update the comment content
-    comment.content = content;
-    await comment.save();
+export const updateComment = async (req, res) => {
+	const commentId = req.params.id;
+	const { content } = req.body;
+	try {
+		// Check if the comment exists
+		const comment = await Comment.findById(commentId);
+		if (!comment) {
+			return res.status(404).json({ message: "Comment not found" });
+		}
 
-    // Return the updated comment
-    return res.status(200).json({ comment });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Server error" });
-  }
-  };
+		// Check if the user is the owner of the comment
+		if (comment.user.toString() !== req.user._id.toString()) {
+			return res.status(403).json({
+				message: "You are not authorized to update this comment",
+			});
+		}
+
+		// Update the comment content
+		comment.content = content;
+		await comment.save();
+
+		// Return the updated comment
+		return res.status(200).json({ comment });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Server error" });
+	}
+};
--- src/routes/comment-routes.js
+++ src/routes/comment-routes.js
@@ -1,10 +1,11 @@
-import  express  from "express";
+import express from "express";
 import { isLoggedIn } from "../auth.js";
-import { updateComment} from "../controllers/comment-controller.js";
+import {
+	updateComment,
+	getAllComments,
+} from "../controllers/comment-controller.js";
 const commentRouter = express.Router();
 
 commentRouter.put("/:id", isLoggedIn, updateComment);
-
+commentRouter.get("/", getAllComments);
 export default commentRouter;
-
-
