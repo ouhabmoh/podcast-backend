113f2bb45c34ec59c32774a32b4022645360e0f7
ouhab <mohamed-moh98@live.com>
2023-09-22 22:00:45 +0100

    remove payload limit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ connectDB(); // Call the database connection function
 
 // enabling CORS for any unknown origin(https://xyz.example.com)
 app.use(cors());
-app.use(express.json({ limit: "500mb" }));
+app.use(express.json());
 
 app.use(router);
 
