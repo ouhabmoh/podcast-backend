a1fecb6c29ae986fc941ab54cff5ce20abeee95b
ouhab <mohamed-moh98@live.com>
2024-01-29 21:48:12 +0100

    added jenkins

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build and Test') {
+            steps {
+                script {
+                    // Run npm commands for testing
+                    sh 'npm install'
+                    sh 'npm test'
+                }
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    // Build the Docker image
+                    sh 'docker-compose build'
+                }
+            }
+        }
+
+        stage('Run Docker Compose') {
+            steps {
+                script {
+                    // Run Docker Compose to start your application
+                    sh 'docker-compose up -d'
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Cleanup: Stop and remove containers after the pipeline finishes
+            script {
+                sh 'docker-compose down'
+            }
+        }
+    }
+}
